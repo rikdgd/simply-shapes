edc0edf299cc5ce2b56c6b4d62e9c6a1807bf165
Rik de Goede <rik.dgd@gmail.com>
2023-07-09 20:38:54 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use pixels::{Error, Pixels, SurfaceTexture};
+use winit::dpi::LogicalSize;
+use winit::event::{Event, VirtualKeyCode};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+const WIDTH: u16 = 800;
+const HEIGHT: u16 = 600;
+
+fn main() {
+    println!("Hello, world!");
+}
