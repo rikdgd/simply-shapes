6a6ca06f076670b694c66c69f26a6ed447dc94be
rikdgd <74911706+rikdgd@users.noreply.github.com>
2023-09-04 23:57:55 +0200

    Added set_main_loop to screen

--- src/window_management.rs
+++ src/window_management.rs
@@ -11,55 +11,82 @@ pub struct Screen<'a>  {
     pub title: String,
     pub width: u16,
     pub height: u16,
-    pub window: Window,
-    pub event_loop: EventLoop<()>,
-    main_loop: &'a dyn FnMut(Screen) -> (),
+    window: Window,
+    pixels: Pixels,
+    event_loop: EventLoop<()>,
+    pub main_loop: &'a mut dyn FnMut(&mut Pixels) -> (),
 }
 
 impl<'a> Screen<'a> {
-    pub fn new(title: &str, width: u16, height: u16, main_loop: &'a dyn FnMut(Screen) -> ()) -> Self {
+    pub fn new(title: &str, width: u16, height: u16, main_loop: &'a mut dyn FnMut(&mut Pixels) -> ()) -> Self {
         let logical_size = LogicalSize::new(width, height);
         let event_loop = EventLoop::new();
+
+        let window = WindowBuilder::new()
+        .with_title(title.clone())
+        .with_inner_size(logical_size)
+        .with_min_inner_size(logical_size)
+        .build(&event_loop)
+        .unwrap();
+
+        let mut pixels = {
+            let window_size = window.inner_size();
+            let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &self.window);
+            Pixels::new(width as u32, height as u32, surface_texture).unwrap()
+        };
+
         
         Self {
             title: String::from(title),
             width,
             height,
-            window: WindowBuilder::new()
-                .with_title(title.clone())
-                .with_inner_size(logical_size)
-                .with_min_inner_size(logical_size)
-                .build(&event_loop)
-                .unwrap(),
+            window,
+            pixels,
             event_loop,
             main_loop,
         }
     }
-    
-    pub fn draw_shape<T>(&self, shape: &T, shape_location: Location, frame: &mut [u8])
-        where 
-            T: Shape
-        {
-        let mut pixels = {
-            let window_size = self.window.inner_size();
-            let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &self.window);
-            Pixels::new(self.width as u32, self.height as u32, surface_texture).unwrap()
-        };
-        
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            let x = (i % self.width as usize) as u16;
-            let y = (i / self.height as usize) as u16;
-            let pixel_location = Location::new(x, y);
 
-            let inside_shape = shape.contains_pixel(&shape_location, &pixel_location);
-            
-            let rgba = if inside_shape {
-                shape.get_color().rgba
-            } else {
-                [0xff, 0xff, 0xff, 0xff]    // Black
-            };
-            
-            pixel.copy_from_slice(&rgba);
-        }
+    pub fn get_window(&self) -> &Window {
+        &self.window
     }
+
+    pub fn set_main_loop(&mut self, main_loop: dyn FnMut(Pixels) -> ()) {
+        self.main_loop = main_loop;
+    }
+
+    pub fn start(&mut self) {
+        self.event_loop.run(move |event, _, control_flow| {
+            // ToDo: check if redraw is requested.
+            self.main_loop(self.pixels);
+            // ToDo: request redraw
+        })
+    }
+    
+    // pub fn draw_shape<T>(&self, shape: &T, shape_location: Location, frame: &mut [u8])
+    //     where 
+    //         T: Shape
+    //     {
+        // let mut pixels = {
+        //     let window_size = self.window.inner_size();
+        //     let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &self.window);
+        //     Pixels::new(self.width as u32, self.height as u32, surface_texture).unwrap()
+        // };
+        
+    //     for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+    //         let x = (i % self.width as usize) as u16;
+    //         let y = (i / self.height as usize) as u16;
+    //         let pixel_location = Location::new(x, y);
+
+    //         let inside_shape = shape.contains_pixel(&shape_location, &pixel_location);
+            
+    //         let rgba = if inside_shape {
+    //             shape.get_color().rgba
+    //         } else {
+    //             [0xff, 0xff, 0xff, 0xff]    // Black
+    //         };
+            
+    //         pixel.copy_from_slice(&rgba);
+    //     }
+    // }
 }
